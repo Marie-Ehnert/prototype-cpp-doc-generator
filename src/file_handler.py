7fc0323556ccfba788733b3519efb9120054b2da
Marie-Ehnert <Marie.Ehnert@jambit.com>
2024-06-23 14:42:08 +0200

    change structure

--- src/file_handler.py
+++ src/file_handler.py
@@ -19,18 +19,22 @@ class FileHandler:
     def extract_function_definitions(self, tree: Tree) -> list:
         list = []
         query_function_definition = self.cpp_language.query(query.FUNCTION_DEFINITION_IDENTIFIER)
-        function_definition_nodes = query_function_definition.captures(tree.root_node)
+        function_definition_info_nodes = query_function_definition.captures(tree.root_node)
         iterator = 0
-        while iterator < len(function_definition_nodes):
-            function_def = [function_definition_nodes[iterator], function_definition_nodes[iterator + 1]]
-            function_info = dict()
-            for tuple in function_def:
+
+        while iterator < len(function_definition_info_nodes):
+            single_function_def = [function_definition_info_nodes[iterator], function_definition_info_nodes[iterator + 1]]
+            function_dto = dict()
+
+            for tuple in single_function_def:
                 match tuple:
                     case (_, "name"):
-                        function_info["name"] = tuple[0].text.decode("utf-8")
+                        function_dto["name"] = tuple[0].text.decode("utf-8")
                     case (_, "param"):
-                        function_info["params"] = tuple[0].text.decode("utf-8")[1: -1].split(",")
+                        function_dto["params"] = tuple[0].text.decode("utf-8")[1: -1].split(",")
+
+            list.append(function_dto)
             iterator += 2
-            list.append(function_info)
+
         print(list)
         return list
